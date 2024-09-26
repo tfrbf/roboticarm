e67dc0ffd7ceb8f7b0e81be1fc98c422604b988b
Taha <amirtahafarshbaf@gmail.com>
2024-09-26 20:50:07 +0330

    updated

--- Codes/Controller/SMC/optimizer.py
+++ Codes/Controller/SMC/optimizer.py
@@ -1,8 +1,28 @@
 import numpy as np
 import random
 from dynamic import *
-from controller import *
 
+# Sliding mode control function (from controller.py)
+def sliding_mode_control(theta, theta_dot, theta_d, lambda1, lambda2, K1, K2, m1, m2, l1, l2):
+    # Tracking errors
+    e = theta - theta_d
+    e_dot = theta_dot
+
+    # Sliding surfaces
+    S1 = e_dot[0] + lambda1 * e[0]
+    S2 = e_dot[1] + lambda2 * e[1]
+
+    # Inertia matrix M
+    A1 = (m1 + m2) * l1**2
+    A2 = m2 * l1 * l2 * np.cos(theta[0] - theta[1])
+    A1_prime = m2 * l2**2
+    M = np.array([[A1, A2], [A2, A1_prime]])
+
+    # Control law
+    tau1 = M[0, 0] * (0 - lambda1 * e_dot[0]) - K1 * np.sign(S1)
+    tau2 = M[1, 1] * (0 - lambda2 * e_dot[1]) - K2 * np.sign(S2)
+
+    return np.array([tau1, tau2]), S1, S2
 
 # Genetic Algorithm parameters
 population_size = 20
