18c1af92b9aa28f88484110321fb272c24f1e8cd
Taha <amirtahafarshbaf@gmail.com>
2024-09-26 20:34:27 +0330

    updated

--- Codes/Controller/SMC/system_parameters.py
+++ Codes/Controller/SMC/system_parameters.py
@@ -4,5 +4,5 @@ m1, m2 = 1.0, 1.0
 l1, l2 = 1.0, 1.0
 g = 9.81
 theta_d1, theta_d2 = np.pi / 2, np.pi / 3  # Desired angles
-lambda1, lambda2 = 15, 15
-K1, K2 = 100, 60
+lambda1, lambda2 = 20, 20
+K1, K2 = 20, 20
